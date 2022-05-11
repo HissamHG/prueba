58e4db7af8d081ef098c9f84d0885cb61939b4c9
hugui <hugui@DESKTOP-PD7KF56>
2022-05-11 15:44:26 -0500

    Primera vez

---
+++ src/main/java/com/hissam/PrimerMV/PrimerMvApplication.java
@@ -0,0 +1,13 @@
+package com.hissam.PrimerMV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerMvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerMvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hissam/PrimerMV/PrimerMvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hissam.PrimerMV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerMvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
